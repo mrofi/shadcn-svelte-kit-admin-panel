7bcc4d5bdb653f92d09700fdcb1e43d1088c35f7
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2025-01-30 06:43:47 +0700

    chore: take out unused files

--- src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import { t } from "@/i18n";
-	import BaseFront from "@/components/layouts/base.front.layout.svelte";
-</script>
-
-<BaseFront class="flex flex-col space-y-6 text-center">
-  <h1 class="text-xl font-semibold">{$t('welcome', {default: 'Shadcn UI + SvelteKit'})}</h1>
-  <p>{$t('visit_docs')}</p>
-</BaseFront>
