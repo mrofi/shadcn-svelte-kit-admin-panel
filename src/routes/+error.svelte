c20597a6b258b3fd1283d810a9787fc8c0caea9b
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2025-02-09 15:45:57 +0700

    feat: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { page } from "$app/state";
+  import BaseFront from "@/components/layouts/front-base.svelte";
+</script>
+
+<BaseFront>
+  <!-- Create error page -->
+   <div class="flex flex-col items-center justify-center h-full">
+    <h1 class="text-2xl font-bold text-center">{page.status}</h1>
+    <h1 class="text-xl font-normal text-center">{page.error?.message}</h1>
+   </div>
+</BaseFront>
