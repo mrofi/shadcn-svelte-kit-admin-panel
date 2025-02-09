060750e6feac6be01b48f1f9095654b7852dde78
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2025-02-09 15:40:58 +0700

    refactor(ui): can modify class in darkmode

--- src/lib/components/ui/darkmode/selector.svelte
+++ src/lib/components/ui/darkmode/selector.svelte
@@ -2,8 +2,12 @@
   import { Moon, Sun } from "lucide-svelte";
   import { Button } from "$lib/components/ui/button";
   import { toggleMode } from "mode-watcher";
+	import { cn } from "@/utils";
+
+  let { class: className } = $props();
 </script>
-<Button onclick={toggleMode} variant="ghost" size="icon" class="hover:bg-transparent">
+
+<Button onclick={toggleMode} variant="ghost" size="icon" class={cn("hover:bg-transparent focus:ring-transparent focus-visible:ring-transparent", className)}>
   <Sun
     class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
   />
